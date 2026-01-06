321d8a64b2afb754ba5c8119e54bab1878d61ba9
Cyrielle Dupire <cydupire@z3r9p4.42lyon.fr>
2026-01-06 10:22:11 +0100

    bubble sort not working

--- testing/ft_is_stack_sorted.c
+++ testing/ft_is_stack_sorted.c
@@ -1,36 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bubble_sort.c                                   :+:      :+:    :+:   */
+/*   ft_is_stack_sorted.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cydupire <cydupire@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/06 09:01:24 by cydupire          #+#    #+#             */
-/*   Updated: 2026/01/06 10:17:07 by cydupire         ###   ########lyon.fr   */
+/*   Created: 2026/01/06 09:02:05 by cydupire          #+#    #+#             */
+/*   Updated: 2026/01/06 09:21:37 by cydupire         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void ft_bubble_sort(t_data *data)
+int	ft_is_stack_sorted(t_data *data)
 {
 	t_list		*ptr;
 
 	ptr = data->a;
-	while (ft_is_stack_sorted(data) != 1)
+	while (ptr->next != NULL)
 	{
-		if (ptr->value > ptr->next->value)
-		{
-			ft_op_swap(&data->a, &data->b, 'a');
-			ft_op_rotate(&data->a, &data->b, 'a');
-			ptr = data->a;
-		}
+		if (ptr->value < ptr->next->value)
+			ptr = ptr->next;
 		else
-		{
-			ft_op_rotate(&data->a, &data->b, 'a');
-			ptr = data->a;
-		}
-		
+			return (0);
 	}
-	return;
+	if (data->b != NULL)
+		return (0);
+	else
+		return (1);
 }
