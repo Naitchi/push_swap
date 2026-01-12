4d1a8a2c90c9a0ee843c4d9d358a9de3ea600569
Naitchi <assasindu39@gmail.com>
2026-01-12 15:02:18 +0100

    fix size condition in adaptive.c

--- adaptive.c
+++ adaptive.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   adaptive.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cydupire <cydupire@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: bclairot <bclairot@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 21:34:32 by bclairot          #+#    #+#             */
-/*   Updated: 2026/01/12 14:57:00 by cydupire         ###   ########lyon.fr   */
+/*   Updated: 2026/01/12 15:02:02 by bclairot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	adaptive(t_data *data, t_bench *bench)
 	// size = data->size_a; // TODO optimize for size and 3 or 5 elements arrays
 	if (data->disorder_rate < 0.2 && data->size_a <= 10)
 		simple(data, bench);
-	else if (data->disorder_rate >= 0.5 || data->size_a <= 500)
+	else if (data->disorder_rate >= 0.5 || data->size_a >= 500)
 		complex(data, bench);
 	else
 		medium(data, bench);
